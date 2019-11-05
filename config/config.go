c660f82fb1d09839819aca7051b9ac2c97ae0c05
Geunyoung Kim <kgysmart@gmail.com>
2019-11-05 10:08:25 +0100

    Implement List Handler

--- config/config.go
+++ config/config.go
@@ -1,1 +1,62 @@
 package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gy-kim/search-service/logging"
+)
+
+const (
+	envDataURL     = "ELASTIC_URL"
+	envServicePort = "SERVICE_PORT"
+)
+
+const (
+	defDataURL     = "http://127.0.0.1:9200"
+	defServicePort = "9000"
+)
+
+// App is the application config
+var App *Config
+
+// Config defines the configuration of app
+type Config struct {
+	servicePort string
+
+	dataURL string
+
+	logger logging.Logger
+}
+
+// BindServicePort returns the host and port this service.
+func (c *Config) BindServicePort() string {
+	return fmt.Sprintf(":%s", c.servicePort)
+}
+
+// Logger returns a reference of logger
+func (c *Config) Logger() logging.Logger {
+	if c.logger == nil {
+		c.logger = &logging.LoggerStdOut{}
+	}
+	return c.logger
+}
+
+// DataURL returns dataURL for elastic.
+func (c *Config) DataURL() string {
+	return c.dataURL
+}
+
+func init() {
+	App = &Config{}
+
+	App.servicePort = getEnvWithDefault(envServicePort, defServicePort)
+	App.dataURL = getEnvWithDefault(envDataURL, defDataURL)
+}
+
+func getEnvWithDefault(key string, defaultVal string) string {
+	if v := os.Getenv(key); v != "" {
+		return v
+	}
+	return defaultVal
+}
--- restful/list_handler.go
+++ restful/list_handler.go
@@ -5,8 +5,27 @@ import (
 	"errors"
 	"io"
 	"net/http"
+	"strconv"
+	"strings"
 
+	"github.com/gorilla/mux"
 	"github.com/gy-kim/search-service/internal/data"
+)
+
+var (
+	errQueryMissing = errors.New("query is missing from requeset")
+)
+
+const (
+	defaultPage = int(0)
+)
+
+const (
+	varPageKey   = "page"
+	varFilterKey = "filter"
+	varQueryKey  = "q"
+	varSortKey   = "sort"
+	varSortAsc   = "sort_asc"
 )
 
 // ListService loads products
@@ -29,7 +48,84 @@ type ListHandler struct {
 }
 
 func (h *ListHandler) ServeHTTP(response http.ResponseWriter, request *http.Request) {
+	query, err := h.extractQuery(request)
+	if err != nil {
+		response.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	filter := h.extractFilter(request)
+	page := h.extractPage(request)
+	sort := h.extractSort(request)
 
+	products, err := h.service.Do(request.Context(), query, filter, sort, page)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	err = h.toJSON(response, products, page)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func (h *ListHandler) extractSort(request *http.Request) *data.SortCond {
+	vars := mux.Vars(request)
+	target, exists := vars[varSortKey]
+	if !exists {
+		return nil
+	}
+
+	asc := true
+
+	if str, exists := vars[varSortAsc]; exists {
+		if str == "false" {
+			asc = false
+		}
+	}
+
+	sort := &data.SortCond{
+		Target:    target,
+		Ascending: asc,
+	}
+	return sort
+}
+
+func (h *ListHandler) extractPage(request *http.Request) int {
+	vars := mux.Vars(request)
+	str, exists := vars[varPageKey]
+	if !exists {
+		return defaultPage
+	}
+	page, err := strconv.Atoi(str)
+	if err != nil {
+		return defaultPage
+	}
+	return page
+}
+
+func (h *ListHandler) extractFilter(request *http.Request) *data.Filter {
+	vars := mux.Vars(request)
+	str, exists := vars[varFilterKey]
+	if !exists {
+		return nil
+	}
+
+	arr := strings.Split(str, ":")
+	if len(arr) != 2 {
+		return nil
+	}
+	filter := &data.Filter{arr[0]: arr[1]}
+	return filter
+}
+
+func (h *ListHandler) extractQuery(request *http.Request) (string, error) {
+	vars := mux.Vars(request)
+	query, exists := vars[varQueryKey]
+	if !exists {
+		return "", errQueryMissing
+	}
+	return query, nil
 }
 
 func (h *ListHandler) toJSON(writer io.Writer, products []*data.Product, page int) error {
